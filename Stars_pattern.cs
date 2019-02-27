e0aadd4ca81103de714ddd54122ea0386fdde741
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-27 10:21:21 -0800

    pattern of stars on user input

---
+++ Stars_pattern.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            int number, i, k, count = 1;
+            Console.Write("Enter number of rows\n");
+            number = int.Parse(Console.ReadLine());
+            count = number - 1;
+            for (k = 1; k <= number; k++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= 2 * k - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            count = 1;
+            for (k = 1; k <= number - 1; k++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count++;
+                for (i = 1; i <= 2 * (number - k) - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
