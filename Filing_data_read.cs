ad28a46e75e88a2f17a8cd59646a3bdf0807c3dc
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-28 02:42:19 -0800

    C# filing read data

---
+++ Filing_data_read.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace myprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("text.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            
+
+            for (int i = 0; i < fs.Length; i++)
+            {
+                Console.Write((char)fs.ReadByte());
+            }
+
+
+        }
+    }
+}
