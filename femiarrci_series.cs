02671817367a53cdf0f0fd48b2b6b6283bc6e87b
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-28 06:55:03 -0800

    Femiarrci series

---
+++ femiarrci_series.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = 0;
+            int w = 1;
+            int sum = 1;
+
+            Console.WriteLine(q);
+            Console.WriteLine(w);
+
+            for (int i = 0; i < 10; i++)
+            {
+                sum = q + w;
+                q = w;
+                w = sum;
+                Console.WriteLine(sum);
+            }
+
+            
+
+        }
+        
+    }
+}
+
