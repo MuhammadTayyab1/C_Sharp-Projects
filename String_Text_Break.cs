81bf23089477730c87e12015ede1e20f0eba72d8
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-28 02:43:21 -0800

    String text breaking

---
+++ String_Text_Break.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter text or name");
+            string data = Console.ReadLine();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int a = (int)data[i];
+                Console.WriteLine((char)a);
+            }
+
+        }
+    }
+}
