4b3f40b7919b72dfa1dd427de543b2dd5f05e465
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-28 02:39:23 -0800

    C# projects

---
+++ Salary_caculation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter salary");
+            float salary = float.Parse(Console.ReadLine());
+            float tax;
+            float rent;
+            float expences;
+            float savings;
+
+            if (salary < 15000)
+            {
+                tax = salary * 0.08f;
+                rent = salary * 0.19f;
+                expences = salary * 0.40f;
+                savings = (salary) - (tax + rent + expences);
+            }
+            else if(salary > 15000 && salary <=35000)
+            {
+                tax = salary * 0.10f;
+                rent = salary * 0.20f;
+                expences = salary * 0.45f;
+                savings = (salary) - (tax + rent + expences);
+            }
+            else if (salary > 35000 && salary <= 50000)
+            {
+                tax = salary * 0.14f;
+                rent = salary * 0.20f;
+                expences = salary * 0.45f;
+                savings = (salary) - (tax + rent + expences);
+            }
+            else if (salary > 50000 && salary <= 100000)
+            {
+                tax = salary * 0.18f;
+                rent = salary * 0.20f;
+                expences = salary * 0.47f;
+                savings = (salary) - (tax + rent + expences);
+            }
+            else
+            {
+                tax = salary * 0.20f;
+                rent = salary * 0.25f;
+                expences = salary * 0.47f;
+                savings = (salary) - (tax + rent + expences);
+            }
+            Console.Clear();
+            Console.WriteLine("========================================");
+            Console.WriteLine("Total Salary  Rs = "+salary);
+            Console.WriteLine("Tax           Rs = "+tax);
+            Console.WriteLine("Rent          Rs = "+rent);
+            Console.WriteLine("Expences      Rs = "+expences);
+            Console.WriteLine("Savings       Rs = "+savings);
+            Console.WriteLine("========================================");
+        }
+    }
+}
---
+++ Vowels_detection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter character");
+            string hold = Console.ReadLine().ToUpper();
+
+            if ((int)hold[0] >= 48 && (int)hold[0] <= 57)
+            {
+                Console.WriteLine("please not enter a number");
+            }
+            else
+            {
+                switch (hold)
+                {
+                    case "A":
+                        Console.WriteLine("vowel");
+                        break;
+                    case "E":
+                        Console.WriteLine("vowel");
+                        break;
+                    case "I":
+                        Console.WriteLine("vowel");
+                        break;
+                    case "O":
+                        Console.WriteLine("vowel");
+                        break;
+                    case "U":
+                        Console.WriteLine("vowel");
+                        break;
+                    default:
+                        Console.WriteLine("not vowel");
+                        break;
+                }
+            }
+            
+
+
+        }
+    }
+}
