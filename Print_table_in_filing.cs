ab74eaf0f5d4e34ab2735d8fb3810cfaa128ec28
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-28 02:53:09 -0800

    print table in filing

---
+++ Print_table_in_filing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace myprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("file.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+
+            Console.WriteLine("Enter number");
+            int num = int.Parse(Console.ReadLine());
+            string data="";
+
+            for (int i = 1; i <=10; i++)
+            {
+                data +=(num+ "   X   "+i+"   =   "+(num*i)+"  \t\n");
+            }
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                fs.WriteByte((byte)data[i]);
+            }
+
+        }
+    }
+}
