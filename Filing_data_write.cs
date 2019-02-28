ab645f8deef530579e2174dd8855d8e509b7d081
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-28 02:42:54 -0800

    C# filing data write

---
+++ Filing_data_write.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace myprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("text.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            string yourdata = "This is your data file";
+
+            for (int i = 0; i < yourdata.Length; i++)
+            {
+                fs.WriteByte((byte)yourdata[i]);
+            }
+
+
+        }
+    }
+}
