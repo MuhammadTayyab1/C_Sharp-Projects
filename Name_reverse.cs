f8665f117e1a09425d4fdbcfdaaf6c77cebbed89
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-28 06:54:40 -0800

    Name reverse C#

---
+++ Name_reverse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string revrse = " ";
+            string name = "zubair";
+            int ind = name.Length - 1;
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                revrse += name[ind];
+                ind--;
+
+            }
+
+            Console.WriteLine(revrse);
+
+
+
+        }
+    }
+}
+
