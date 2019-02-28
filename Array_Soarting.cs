6cb34d1ecae712eb78cb41eead2b8d801ab1f16c
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-28 02:41:47 -0800

    soarting data in arrays

---
+++ Array_Soarting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int[] store = new int[1000];
+
+            for (int i = 0; i < store.Length; i++)
+            {
+                store[i] = r.Next(1, 1000);
+            }
+            Array.Sort(store);
+
+            for (int i = 0; i < store.Length; i++)
+            {
+                Console.WriteLine(store[i]);
+            }
+
+
+        }
+    }
+}
