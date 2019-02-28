3ae0433f3ed5f03c3e27f7b5278330129d6ad0c8
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-28 02:40:42 -0800

    Data entry in arrays

---
+++ Data_entry_arrays.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many workers");
+            int w = Convert.ToInt32(Console.ReadLine());
+
+
+            string[,] record = new string[w, 4];
+
+
+            for (int i = 0; i < record.GetLength(0); i++)
+            {
+                Console.WriteLine("Enter name : ");
+                record[i, 0] = "Name    : "+Console.ReadLine();
+
+                Console.WriteLine("Enter contact number : ");
+                record[i, 1] = "contact : " + Console.ReadLine();
+
+                Console.WriteLine("Enter Email : ");
+                record[i, 2] = "Email   : " + Console.ReadLine();
+
+                Console.WriteLine("Enter Adress : ");
+                record[i, 3] = "Adress  : " + Console.ReadLine();
+                Console.WriteLine("");
+            }
+            Console.Clear();
+
+
+            for (int i = 0; i < record.GetLength(0); i++)
+            {
+                for (int j = 0; j < record.GetLength(1); j++)
+                {
+                    Console.WriteLine(record[i,j]);
+                }
+            }
+
+
+        }
+    }
+}
