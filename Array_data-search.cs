2bd52d4aa21cd670a370326e70ff2095a5487d1b
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-28 02:41:18 -0800

    array-data-search

---
+++ Array_data-search.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int[] store = new int[10000];
+
+            for (int i = 0; i < store.Length; i++)
+            {
+                store[i] = r.Next(1, 100);
+            }
+
+            Console.WriteLine("enter any number from 1 to 99");
+            int check = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < store.Length; i++)
+            {
+                if(store[i]==check)
+                {
+                    Console.WriteLine(check+" is store in array number  store["+i+"]");
+                }
+            }
+
+
+        }
+    }
+}
