2ee4a301d3c3e80249169896ac8c66b51e8d8a96
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-03-07 08:09:41 -0800

    Prime numbers detector

---
+++ prime_number_detector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication285
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter ");
+            int number = int.Parse(Console.ReadLine());
+
+            bool flag = false;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i ==0)
+                {
+                    flag = true;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (flag==false)
+            {
+                Console.WriteLine(number+" is a prime");
+            }
+            else
+            {
+                Console.WriteLine(number+ " is not a prime");
+            }
+
+        }
+    }
+}
