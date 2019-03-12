06f4e44c1f4dfdecabd94dea08b9c9875821af98
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-03-12 11:24:59 -0700

    Factorial

---
+++ Factorial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication291
+{
+    class factorial
+    {
+        int num = 1;
+        public factorial(int n)
+        {
+            
+            for (int i = 1; i <= n ; i++)
+            {
+                num  *= i;
+            }
+            Console.WriteLine("Factorial of "+n+" is  =  "+num );
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            factorial obj = new factorial(num);
+        }
+
+
+    }
+}
