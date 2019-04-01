9c940620b8399ee7a66f803f6936f2fad65f0257
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-04-01 12:13:18 -0700

    copy constructor

---
+++ Copy_Constructor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copycontrustor
+{
+
+    class data
+    {
+        string name;
+        int id;
+
+        // original construstor
+        public data(string n,int i)
+        {
+            name = n;
+            id = i;
+        }
+
+        // copy construstor 
+        public data(data a)
+        {
+            name = a.name;
+            id = a.id;
+        }
+        public void show()
+        {
+            Console.WriteLine("Name   : "+name);
+            Console.WriteLine("Id     : "+id);
+            Console.WriteLine("--------------------------");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // original
+            data obj = new data("ali", 1234);
+            obj.show();
+
+            // copy 
+            data copy = new data(obj);
+            copy.show();
+
+        }
+    }
+}
