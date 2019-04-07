bd76f7a24ce64112edd3c1baba33eaaf00e72557
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-04-07 13:30:23 -0700

    Quiz application on console

--- Program.cs
+++ Program.cs
@@ -4,30 +4,139 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication303
+namespace quiz
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Your Text");
-            string text = Console.ReadLine();
-
-            int len = text.Length;
-
-            string [] data= new string [len];
-
-
-            for (int i = 0; i < text.Length; i++)
+            try
             {
-                data[i] = Convert.ToString(text[i]);
+                int score = 0;
+                int count = 0;
+
+                // Question 1
+                Console.WriteLine("Capital of pakistan is");
+                Console.WriteLine("1) Karachi ");
+                Console.WriteLine("2) Islamabad");
+                Console.WriteLine("\nEnter option 1 or 2 for skip press 0");
+                int p1 = int.Parse(Console.ReadLine());
+                if (p1 != 0)
+                {
+                    if (p1 == 2)
+                    {
+                        score += 100;
+                        count += 1;
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+                }
+
+                Console.Clear();
+
+                // Question 2
+                Console.WriteLine("CPU stands for");
+                Console.WriteLine("1) Central processing unit ");
+                Console.WriteLine("2) Control panel unit");
+                Console.WriteLine("\nEnter option 1 or 2 for skip press 0");
+                int p2 = int.Parse(Console.ReadLine());
+
+                if (p2 != 0)
+                {
+                    if (p2 == 1)
+                    {
+                        score += 100;
+                        count += 1;
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+                }
+                Console.Clear();
+
+                // Question 3
+                Console.WriteLine("What is computer");
+                Console.WriteLine("1) electronic machine ");
+                Console.WriteLine("2) heat engine");
+                Console.WriteLine("\nEnter option 1 or 2 for skip press 0");
+                int p3 = int.Parse(Console.ReadLine());
+
+                if (p3 != 0)
+                {
+                    if (p3 == 1)
+                    {
+                        score += 100;
+                        count += 1;
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+                }
+                Console.Clear();
+
+                // Question 4
+                Console.WriteLine("IOT stands for");
+                Console.WriteLine("1) Inter operating technology ");
+                Console.WriteLine("2) Internet of things");
+                Console.WriteLine("\nEnter option 1 or 2 for skip press 0");
+                int p4 = int.Parse(Console.ReadLine());
+
+                if (p4 != 0)
+                {
+                    if (p4 == 2)
+                    {
+                        score += 100;
+                        count += 1;
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+                }
+                Console.Clear();
+
+                // Question 5/
+                Console.WriteLine("Standard limit SAR value of mobile is");
+                Console.WriteLine("1) 1.5 - 2.0 ");
+                Console.WriteLine("2) 3.0 - 4.0");
+                Console.WriteLine("\nEnter option 1 or 2 for skip press 0");
+                int p5 = int.Parse(Console.ReadLine());
+
+                if (p5 != 0)
+                {
+                    if (p5 == 1)
+                    {
+                        score += 100;
+                        count += 1;
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+                }
+
+
+
+
+
+
+                Console.Clear();
+                Console.WriteLine("Your score is  " + score);
+                Console.WriteLine("Total attempt quiz   " + count);
             }
-            string inc = "";
-            for (int i = 0; i < data.Length; i++)
+            catch (Exception ex)
             {
-                Console.WriteLine(inc+"* "+data[i]);
-                inc += "*";
+                Console.WriteLine(ex.Message);
             }
+
+
+
+
+
         }
     }
 }
