f7a5e658f9287bb6c55492733cabc24621ee3c3f
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-04-21 13:47:53 -0700

    guessing game

---
+++ C# OOP/Program2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+   class game
+   {
+       static int count;
+       public game(int n)
+       {
+           Random r = new Random();
+           int ran = r.Next(1, 5);
+
+           if(ran==n)
+           {
+               count++;
+           }
+       }
+       public static void result()
+       {
+           if(count >= 3)
+           {
+               Console.WriteLine("You guess 3 or more You win ");
+           }
+           else
+           {
+               Console.WriteLine("You loss");
+           }
+       }
+   }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            game[] obj = new game[10];
+
+            for (int i = 0; i < obj.Length; i++)
+            {
+                Console.WriteLine("Enter number or type break to stop");
+                string userdata = Console.ReadLine();
+                if (userdata == "break")
+                {
+                    break;
+                }
+                else
+                {
+                    int user = Convert.ToInt32(userdata);
+                    obj[i] = new game(user);
+                }
+            }
+
+            game.result();
+           
+        }
+    }
+}
